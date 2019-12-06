f41ba782276ac164aac55eb5d8e1f2f8ba4d42f3
Joe Allen <allenjosephs@johndeere.com>
2019-12-06 09:17:36 -0600

    fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -2,26 +2,34 @@
 
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
-
+var foods = ["apple", "steak", "potato"];
 
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
+var last = foods[2];
 
 
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
-
+var favoriteFoods = [];
 
 
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
-
+for (let i = 0; i < foods.length; i++) {
+  favoriteFoods[i] = foods[i];
+}
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
+var instructor = {
+  name: "Dave",
+  age: 23,
+  sex: "m"
+}
 
 
 
@@ -29,3 +37,5 @@
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor["has-office-hours"] = false;
+
