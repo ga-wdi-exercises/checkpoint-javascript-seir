b24643f967d5847ddd70453089326b5fa0fd538e
KASSIDY_SHADE <kassie2129@gmail.com>
2019-12-06 09:59:58 -0600

    finished as much as I could

--- hof.js
+++ hof.js
@@ -21,7 +21,7 @@ var people = [
 // called `peopleNames`.
 // Type your solution immediately below this line:
 
-
+var peopleNames = peopleNames.push(people.map(people.name))
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who know multiple languages. Assign the returned array
